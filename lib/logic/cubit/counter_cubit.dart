f3556778bf8dabd587f38b4cd65b4f619c964a86
Samuel Matias <hi@samuelematias.com>
2021-03-02 07:32:25 -0300

    feat(hydrated_bloc): implement the hydratec_bloc

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
-class CounterCubit extends Cubit<CounterState> {
+class CounterCubit extends Cubit<CounterState> with HydratedMixin {
   CounterCubit() : super(const CounterState(counterValue: 0));
 
   void increment() => emit(
@@ -12,4 +15,14 @@ class CounterCubit extends Cubit<CounterState> {
 
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
+
+  @override
+  CounterState fromJson(Map<String, Object> json) {
+    return CounterState.fromJson(json);
+  }
+
+  @override
+  Map<String, Object> toJson(CounterState state) {
+    return state.toMap();
+  }
 }
